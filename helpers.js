4fa2f39f5d0162befdb916b1b3f644bd682163f3
Nicolas Chambrier <naholyr@gmail.com>
2017-02-10 14:18:37 +0100

    Helper 'parseAddresses' now also parses 'from' header

--- helpers.js
+++ helpers.js
@@ -17,17 +17,36 @@ exports.parseAddresses = (headers, { quiet = false } = {}) => {
 	_parseAddressHeader(headers, 'to', quiet)
 	_parseAddressHeader(headers, 'cc', quiet)
 	_parseAddressHeader(headers, 'bcc', quiet)
+	_parseAddressHeader(headers, 'from', quiet, false)
 	return headers
 }
 
-const _parseAddressHeader = (headers, field, quiet = false) => {
-	let addresses = headers[field]
-	if (typeof addresses === 'string') {
-		addresses = [addresses]
-	} else if (!addresses) {
-		addresses = []
+const _parseAddressHeader = (headers, field, quiet = false, multiple = true) => {
+	if (multiple) {
+		let addresses = headers[field]
+		if (typeof addresses === 'string') {
+			addresses = [addresses]
+		} else if (!addresses) {
+			addresses = []
+		}
+		headers[field] = addresses.map(address => _parseAddressValue(address, quiet))
+	} else {
+		let value = headers[field]
+		if (Array.isArray(value)) {
+			if (value.length === 0) {
+				headers[field] = null
+				return
+			} else if (value.length === 1) {
+				value = value[0]
+			} else {
+				debug('Error parsing address: expecting non-multiple values and got', value)
+				if (!quiet) {
+					throw new Error('Non multiple value expected for header "' + field + '"')
+				}
+			}
+		}
+		headers[field] = _parseAddressValue(value)
 	}
-	headers[field] = addresses.map(address => _parseAddressValue(address, quiet))
 }
 
 const _parseAddressValue = (value, quiet = false) => {
